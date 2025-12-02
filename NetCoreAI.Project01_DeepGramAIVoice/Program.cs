c75992ba44e1a6dff2e770d44daaa67f57c3e26a
Ergin TIRAVOGLU <ergintiravoglu@msn.com>
2025-12-02 19:33:20 +0300

    Add Hugging Face sentiment analysis project and HTML interface
    - Implemented sentiment analysis using Hugging Face's Twitter RoBERTa model in Program.cs.
    - Created a new HTML file for the project interface with relevant metadata and styling.
    - Added solution file to include the new project in the .NET solution structure.

---
+++ NetCoreAI.Project01_DeepGramAIVoice/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Net.Http.Headers;
+using System.Text.Json;
+
+var apiKey="7aa21f092ba058e093e889b7ee6fdfb8421f58cf";
+var filePath="./files/testeng.mp3";
+var filePathTr="./files/testtr.mp3";
+
+if(!File.Exists(filePathTr))
+{
+    Console.WriteLine($"File not found: {filePathTr}");
+    return;
+}
+
+using var httpClient=new HttpClient();
+httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Token", apiKey);
+
+using var fileStream=File.OpenRead(filePathTr);
+
+var content = new StreamContent(fileStream);
+content.Headers.ContentType = new MediaTypeHeaderValue("audio/mp3");
+
+var response = await httpClient.PostAsync("https://api.deepgram.com/v1/listen?punctuate=true&language=tr", content);
+var json = await response.Content.ReadAsStringAsync();
+
+try
+{
+    var doc = JsonDocument.Parse(json);
+    var transcript = doc.RootElement
+        .GetProperty("results")
+        .GetProperty("channels")[0]
+        .GetProperty("alternatives")[0]
+        .GetProperty("transcript")
+        .GetString();
+    Console.WriteLine("");
+    Console.WriteLine("Transcription:");
+    Console.WriteLine(transcript);
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Failed to parse JSON response:");
+    Console.WriteLine(ex.Message);
+    Console.WriteLine("Response content: \n" + json);
+    return;
+}
---
+++ NetCoreAI.Project02_HuggingFaceSentimentAnalysis/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Globalization;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+
+var apiKey ="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";
+
+var text = "This movie was absolutely fantastic! I love every second of it.";
+var modelUrl = "https://router.huggingface.co/hf-inference/models/cardiffnlp/twitter-roberta-base-sentiment";
+
+using var httpClient = new HttpClient();
+httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiKey);
+
+var json=JsonSerializer.Serialize( new { inputs = text });
+var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+var response = await httpClient.PostAsync(modelUrl, content);
+var result = await response.Content.ReadAsStringAsync();
+
+var doc = JsonDocument.Parse(result);
+var items = doc.RootElement[0];
+
+var topLabel = items
+    .EnumerateArray()
+    .OrderByDescending(x => x.GetProperty("score").GetDouble())
+    .First();
+
+var label = topLabel.GetProperty("label").GetString();
+var score = topLabel.GetProperty("score").GetDouble();
+
+string labelText = label switch
+{
+    "LABEL_0" => "Negative",
+    "LABEL_1" => "Neutral",
+    "LABEL_2" => "Positive",
+    _ => "Unknown"
+};
+
+Console.OutputEncoding = Encoding.UTF8;
+Console.WriteLine($"Girdi Metni: {text}");
+
+Console.WriteLine($"Duygu Analizi: ");
+Console.WriteLine($"Duygu Durumu: {labelText}");
+Console.WriteLine($"Güven Skoru: {(score * 100).ToString("F2", CultureInfo.InvariantCulture)}");
